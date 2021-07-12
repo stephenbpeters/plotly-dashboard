40fc943b549898a00809c63c59c31abb6b24bd51
stephenbpeters <stephen.peters@gmail.com>
2021-07-12 15:55:14 -0700

    progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,24 @@
-// read in our data, print to console - works
-// d3.json("./data/samples.json", function (d) {
-// 	console.log(d);
-// });
+// javascript plotly homework
 
+// point ourselves to our json data file
+var url = "./samples.json";
 
-
-// taking from video of class
-var url = "./data/samples.json";
-
-function buildPlot() {
-	d3.json(url, function (data) {
-		console.log(data);
-	});
+// build and populate our drop-down with our individuals
+function buildDrop() {
+    d3.json(url, function (data) {
+        console.log(data);
+        // build drop down from data
+        d3.select('select')
+            .selectAll('option')
+            .data(data.names)
+            .enter()
+            .append('option')
+            .text(function (d) { return d; });
+        // end of dropdown	 
+        console.log('hey, show our data please', data);
+    });
+    console.log('dropdown built');
+    
 };
 
-
-// getting data again, built stuff for plot	
-
-// function buildPlot() {
-// 	d3.json(url).then(function(data) {
-// 	  console.log(data);
-// 	  // get values
-// 	  var info = data.samples;
-// 	  console.log("show data.samples");
-// 	  console.log(info);
-// 	});	
-// 	
-// 	};
-	
-buildPlot();	
-console.log("test one?");
-console.log('really');
+buildDrop();
---
+++ static/js/bonus.js
@@ -0,0 +1,1 @@
+// potential bonus section
